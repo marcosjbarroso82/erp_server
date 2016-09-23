ca5d8a18a0b49cb4e11a1c0ccc71baa0ea93aa47
Marcos <marcosjbarroso82@gmail.com>
2016-09-23 12:58:02 -0300

    add item to order

--- apps/order/models.py
+++ apps/order/models.py
@@ -20,6 +20,75 @@ class Order(BaseModel):
     total = models.DecimalField(decimal_places=2, max_digits=12, editable=False, default=0)
 
 
+    def add_item(self, product, quantity, data={}, action='increment'):
+        # TODO: borrar el item si fue modificado
+        # try:
+
+        if product:
+            # msg = ''
+            # Checkear si otro item con la misma variacion ya reservo stock
+            reserved_stock = 0
+            for item in self.items.all():
+                if item.product == product:
+                    reserved_stock = reserved_stock + item.quantity
+
+            stocked_quantity = product.get_stock_quantity()
+            available_quantity = stocked_quantity - reserved_stock
+
+            if action == 'increment':
+                for item in self.items.all():
+                    if item.product == product:
+                        if quantity > available_quantity:
+                            item.quantity = item.quantity + available_quantity
+                            # item.msg = 'No hay suficiente stock para este item. Hemos ingresado para este item, el maximo disponible'
+                        else:
+                            item.quantity = item.quantity + quantity
+                        break
+                else: # this else belongs to the for loop and it's executed only if break instruction has no been executed.
+                    if quantity > available_quantity:
+                        new_quantity = available_quantity
+                        # msg = 'No hay suficiente stock para este item. Hemos ingresado para este item, el maximo disponible'
+                    else:
+                        new_quantity = quantity
+                    if new_quantity > 0:
+                        item = OrderItem(order=self, product=product, quantity=new_quantity, price=product.get_price_per_item())
+                        item.product = product
+                        # self.items.append(item)
+
+            elif action == 'add':
+                if quantity > available_quantity:
+                    new_quantity = available_quantity
+                    # msg = 'No hay suficiente stock para este item. Hemos ingresado para este item, el maximo disponible'
+                else:
+                    new_quantity = quantity
+                if new_quantity > 0:
+                    item = OrderItem(order=self, product=product, quantity=new_quantity, price=product.get_price_per_item())
+                    item.product = product
+                    # self.items.append(item)
+            elif action == 'replace':
+                for item in self.items.all():
+                    if item.product == product:
+                        if quantity > available_quantity:
+                            item.quantity = available_quantity
+                            # item.msg = 'No hay suficiente stock para este item. Hemos ingresado para este item, el maximo disponible'
+                        else:
+                            item.quantity = quantity
+                        break
+                else: # this else belongs to the for loop and it's executed only if break instruction has no been executed.
+                    if quantity > available_quantity:
+                        new_quantity = available_quantity
+                        # msg = 'No hay suficiente stock para este item. Hemos ingresado para este item, el maximo disponible'
+                    else:
+                        new_quantity = quantity
+                    if new_quantity > 0:
+                        item = OrderItem(order=self, product=product, quantity=new_quantity, price=product.get_price_per_item())
+                        item.product = product
+                        # self.items.append(item)
+            # import ipdb; ipdb.set_trace()
+            item.save()
+        
+
+
 class OrderItem(BaseModel):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
--- apps/order/serializers.py
+++ apps/order/serializers.py
@@ -21,5 +21,6 @@ class OrderSerializer(serializers.ModelSerializer):
         items_data = validated_data.pop('items')
         order = Order.objects.create(**validated_data)
         for item_data in items_data:
-            OrderItem.objects.create(order=order, **item_data)
+            # OrderItem.objects.create(order=order, **item_data)
+            order.add_item(**item_data)
         return order
