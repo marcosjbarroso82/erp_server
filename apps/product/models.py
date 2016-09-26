192151660aa34dd6b96faf5d8289cd53e2b351dc
Matias Roson <matiroson@gmail.com>
2016-09-26 15:01:29 -0300

    Added product description

--- apps/product/models.py
+++ apps/product/models.py
@@ -6,9 +6,10 @@ class Product(BaseModel):
     sku = models.CharField(max_length=20)
     name = models.CharField(max_length=20)
     price = models.DecimalField(decimal_places=2, max_digits=12) # validators=[models.validators.MinValueValidator(0)])
+    description = models.TextField()
 
     def get_stock_quantity(self):
-        return 5
+        return self.stock.quantity
 
     def get_price_per_item(self):
         return 7
