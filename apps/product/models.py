70e7bbfa786d3db829dc83335141a8f9238ca44a
Marcos <marcosjbarroso82@gmail.com>
2016-09-26 18:09:51 -0300

    product stock

--- apps/product/models.py
+++ apps/product/models.py
@@ -15,13 +15,24 @@ class Product(BaseModel):
     def reserved_stock(self):
         return self.get_reserved_stock()
 
+    @property
+    def available_stock(self):
+        return self.get_available_stock()
+
+    @property
+    def stock_quantity(self):
+        return self.get_stock_quantity()
 
     def get_stock_quantity(self):
         return self.stock.quantity
 
     def get_reserved_stock(self):
         reserved = OrderItem.objects.filter(product=self, order__status=1).aggregate(Sum('quantity'))
-        return reserved.get('quantity__sum', 0)
+        # import ipdb; ipdb.set_trace()
+        if not reserved.get('quantity__sum'):
+            return 0
+        else:
+            return reserved.get('quantity__sum')
 
     def get_available_stock(self):
         return self.get_stock_quantity() - self.get_reserved_stock()
--- apps/product/serializers.py
+++ apps/product/serializers.py
@@ -4,6 +4,8 @@ from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     reserved_stock = serializers.FloatField(read_only=True)
+    available_stock = serializers.FloatField(read_only=True)
+    stock_quantity = serializers.FloatField(read_only=True)
 
     class Meta:
         model = Product
